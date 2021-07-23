43074c944f159a1adb437e7c40a54dca9ae2802a
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-07-23 13:58:20 +0300

    Updated scripts

--- SQL Migration/Bonava/Table232.GenJournalBatch [Bonava].sql
+++ SQL Migration/Bonava/Table232.GenJournalBatch [Bonava].sql
@@ -1,6 +1,7 @@
 DELETE FROM [Bonava-Test].[dbo].[Bonava$Gen_ Journal Batch$437dbf0e-84ff-417a-965d-ed2bb9650972];
 
 --Gen Journal Batch
+--Base Table
 INSERT INTO [Bonava-Test].[dbo].[Bonava$Gen_ Journal Batch$437dbf0e-84ff-417a-965d-ed2bb9650972]
 (
 	[Journal Template Name],
@@ -20,9 +21,11 @@ SELECT
 	[Description],
 	[Reason Code],
 	[Bal_ Account Type],
-	[Bal_ Account No_],
+	ISNULL(GLAccMapping.[New No_], '') AS [Bal_ Account No_],
 	[No_ Series],
 	[Posting No_ Series],
 	[Copy VAT Setup to Jnl_ Lines],
 	[Allow VAT Difference]
-FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[Bonava$Gen_ Journal Batch];
+FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[Bonava$Gen_ Journal Batch]
+LEFT JOIN [Bonava-Test].[dbo].[Bonava$G_L Account Mapping$2944687f-9cf8-4134-a24c-e21fb70a8b1a] GLAccMapping
+ON GLAccMapping.[Old No_] = [Bal_ Account No_] collate Cyrillic_General_100_CI_AS;
