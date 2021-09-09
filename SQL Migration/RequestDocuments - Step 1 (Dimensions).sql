e268621f24737a3678902c57406d03ad61cb72cf
AlexVB77 <abatiukov@gmail.com>
2021-09-09 15:32:29 +0300

    54697

--- SQL Migration/RequestDocuments - Step 1 (Dimensions).sql
+++ SQL Migration/RequestDocuments - Step 1 (Dimensions).sql
@@ -26,18 +26,31 @@ TRUNCATE TABLE #PivotDimDoc
 
 INSERT #DocList ([DocNo])
 	SELECT PH.[No_]
-	FROM [VM-PRO-SQL007\NAV].[NAV_Dev].dbo.[NCC Construction$Purchase Header] PH  
-	INNER JOIN [VM-PRO-SQL007\NAV].[NAV_Dev].dbo.[NCC Construction$Purchase Header Additional] PHA
+	FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].dbo.[NCC Construction$Purchase Header] PH  
+	INNER JOIN [VM-PRO-SQL007\NAV].[NAV_for_Developers].dbo.[NCC Construction$Purchase Header Additional] PHA
 		ON PHA.[Document Type] = PH.[Document Type] and PHA.[No_] = PH.[No_]
 	WHERE PH.[Document Type] = 1 and PH.[Act Type] <> 0 --and PH.[Problem Document] = 0 and PHA.[Status App Act] between 2 and 5 and PH.Archival = 0
 
-/*
-DELETE FROM [MSK10NAV65].[SimpleERP].dbo.[' + @Company + '$ImportDimPivot] WHERE TableID = 25 
-INSERT [MSK10NAV65].[SimpleERP].dbo.[' + @Company + '$ImportDimPivot]
+DELETE FROM [VM-TST-SQL013].[Bonava-Test].dbo.[Bonava$ImportDimPivot$2944687f-9cf8-4134-a24c-e21fb70a8b1a] WHERE [TableID] IN (38,39) 
+INSERT [VM-TST-SQL013].[Bonava-Test].dbo.[Bonava$ImportDimPivot$2944687f-9cf8-4134-a24c-e21fb70a8b1a]
 	(TableID,Dim1Code,Dim2Code,Dim3Code,Dim4Code,Dim5Code,Dim6Code,Dim7Code,Dim8Code,Dim9Code,Dim10Code)
-VALUES
-	(25,''бхдш пюяунднб'',''йнд онегдйх'',''лемедфепш'',''опнейр'',''ярюрэъ гюрпюр'',''жтн'',''жтс'',''чпкхжю'','''','''')  
-*/
+	VALUES(38, 'CC','CP','CT','мо','мс-бхд','мс-назейр','рпд','','','') 
+INSERT [VM-TST-SQL013].[Bonava-Test].dbo.[Bonava$ImportDimPivot$2944687f-9cf8-4134-a24c-e21fb70a8b1a]
+	(TableID,Dim1Code,Dim2Code,Dim3Code,Dim4Code,Dim5Code,Dim6Code,Dim7Code,Dim8Code,Dim9Code,Dim10Code)
+	VALUES(39, 'ADDRESS','CC','CP','CT','демднй','хмфяерэ','мо','мс-бхд','мс-назейр','рпд')
+
+;WITH PivotDimEntry AS (
+	SELECT [Entry No_],[бхдш пюяунднб],[йнд онегдйх],[лемедфепш],[опнейр],[ярюрэъ гюрпюр],[жтн],[жтс],[чпкхжю] FROM (
+		SELECT LED.[Dimension Value Code], LED.[Dimension Code], LED.[Entry No_] FROM [MSK10NAV65].[SimpleERPNAV2009].dbo.[' + @Company + '$Ledger Entry Dimension] LED	
+		INNER JOIN #EntryListVend EL ON LED.[Table ID] = 25 AND LED.[Entry No_] = EL.EntryNo) SRC
+	PIVOT (
+		MIN([Dimension Value Code]) FOR [Dimension Code] IN ([бхдш пюяунднб],[йнд онегдйх],[лемедфепш],[опнейр],[ярюрэъ гюрпюр],[жтн],[жтс],[чпкхжю])) AS PVT)
+INSERT #PivotDimEntryVend
+	(EntryNo,SQLDimSetID,D1VC,D2VC,D3VC,D4VC,D5VC,D6VC,D7VC,D8VC,D9VC,D10VC)
+SELECT [Entry No_],0,
+	ISNULL([бхдш пюяунднб],''''),ISNULL([йнд онегдйх],''''),ISNULL([лемедфепш],''''),ISNULL([опнейр],''''),
+	ISNULL([ярюрэъ гюрпюр],''''),ISNULL([жтн],''''),ISNULL([жтс],''''),ISNULL([чпкхжю],''''),'''',''''
+FROM PivotDimEntry
 
 
 DROP TABLE #PivotDimDoc
